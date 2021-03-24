c5bd48bb8bd14da7fceaea5f3fc2273137407dcb
Kleber Motta <kleber.outlook@gmail.com>
2021-03-24 11:58:48 -0300

    removendo teste de role

--- commands/twitch.js
+++ commands/twitch.js
@@ -2,12 +2,6 @@ module.exports = {
   name: 'twitch',
   description: 'Envia o link do nosso canal oficial da twitch!',
   execute(message, args, cmd, client, discord) {
-
-    if (message.member.roles.cache.has('798700941784121384')) {
-      message.channel.send('Siga nosso canal oficial na Twitch - https://www.twitch.tv/mrmenta');
-    } else {
-      message.channel.send('Desculpe, você não tem permissão para usar esse comando');
-    }
-
+    message.channel.send('Siga nosso canal oficial na Twitch - https://www.twitch.tv/mrmenta');
   }
 }
